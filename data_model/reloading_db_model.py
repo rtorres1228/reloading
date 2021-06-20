0bc2c980c432a890b55d21b3e18a2e432817fb3e
Roberto Torres <robertotorres@Robertos-MacBook-Pro.local>
2021-06-20 15:03:19 -0400

    refactor orm classes

--- data_model/reloading_db_model.py
+++ data_model/reloading_db_model.py
@@ -1,11 +1,25 @@
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import Integer, Text, String, Numeric
+from sqlalchemy.schema import ForeignKey, PrimaryKeyConstraint, Table
+from sqlalchemy.orm import relationship
+
 from database_engine import get_database_engine
 
 Base = declarative_base()
 
 DB_ENGINE = get_database_engine()
+
+
+class Contributor(Base):
+    """ contributor """
+
+    __table_name = 'tbl_contributor'
+    id = Column(Integer, primary_key=True, autoincrement="auto")
+    email = Column('email', String(255), primary_key=True, nullable=False)
+    name = Column('name', String(255), nullable=True)
+    calibers = relationship("Caliber")
+
 
 class Caliber(Base):
     """User account."""
@@ -14,22 +28,18 @@ class Caliber(Base):
 
     id = Column(Integer, primary_key=True, autoincrement="auto")
     caliber_name = Column('caliber_name', String(255), unique=True, nullable=False)
+    contributor_id = Column(Integer, ForeignKey('tbl_contributor.id'))
+    load = relationship("Load")
 
-class Weight(Base):
-    """bullet weight"""
-
-    __tablename__ = 'tbl_weight'
-
-    id = Column(Integer, primary_key=True, autoincrement="auto")
-    # bullet weight
-    weight = Column('weight', Numeric, nullable=False)
 
 class Load(Base):
     """load"""
 
     __tablename__ = 'tbl_load'
     id = Column(Integer, primary_key=True, autoincrement="auto")
-    load_name = caliber_name = Column('load_name', String(255), unique=True, nullable=False)
+    caliber_id = Column(Integer, ForeignKey='tbl_caliber.id')
+    weight = Column('weight', Numeric, nullable=False)
+    load_name = Column('load_name', String(255), unique=True, nullable=True)
     powder_charge = Column('powder_charge', Integer, nullable=False)
     # R or P
     primer_type = Column('primer_type', String(1), nullable=False)
@@ -42,9 +52,5 @@ class Load(Base):
     # power factor
     PF = Column('PF', Numeric, nullable=True)
 
-class Contributor(Base):
-    """ contributor """
-    __table_name = 'tbl_contributor'
-    id = Column(Integer, primary_key=True, autoincrement="auto")
-    email = Column('email', String(255), nullable=False)
-    name = Column('name', String(255), nullable=True)
+
+
